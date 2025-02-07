b876ddbb09e055b43f2c17aa9eca6efe54f27687
wombatgamery <81746151+wombatgamery@users.noreply.github.com>
2025-02-07 22:23:26 +1100

    Update BuiltinRemnants.cs
    up to date

--- BuiltinModSupport/BuiltinRemnants.cs
+++ BuiltinModSupport/BuiltinRemnants.cs
@@ -11,14 +11,17 @@ public class BuiltinRemnants : AutoImplementedModSupport
     {
         miniBiomes = new Dictionary<string, BiomeEntry>
         {
-            {"GoldenCity",   new BiomeEntry{ Title = "Golden City",   SubTitle = "Remnants", TitleColor = Color.Gold,        StrokeColor = Color.Black }},
-            {"Growth",       new BiomeEntry{ Title = "Growth",        SubTitle = "Remnants", TitleColor = Color.ForestGreen, StrokeColor = Color.Black }},
-            {"OceanCave",    new BiomeEntry{ Title = "Ocean Cave",    SubTitle = "Remnants", TitleColor = Color.HotPink,     StrokeColor = Color.Black }},
-            {"Pyramid",      new BiomeEntry{ Title = "Pyramid",       SubTitle = "Remnants", TitleColor = Color.Goldenrod,   StrokeColor = Color.Black }},
-            {"Hive",         new BiomeEntry{ Title = "Giant Hive",    SubTitle = "Remnants", TitleColor = Color.Yellow,      StrokeColor = Color.Black }},
-            {"JungleTemple", new BiomeEntry{ Title = "Jungle Temple", SubTitle = "Remnants", TitleColor = Color.DarkGreen,   StrokeColor = Color.Black }},
             {"MarbleCave",   new BiomeEntry{ Title = "Marble Cave",   SubTitle = "Remnants", TitleColor = Color.LightGray,   StrokeColor = Color.Black }},
-            {"Vault",        new BiomeEntry{ Title = "Vault",         SubTitle = "Remnants", TitleColor = Color.Gold,        StrokeColor = Color.Black }},
+            {"GraniteCave",  new BiomeEntry{ Title = "Granite Cave",  SubTitle = "Remnants", TitleColor = Color.SkyBlue,     StrokeColor = Color.Black }},
+            {"OceanCave",    new BiomeEntry{ Title = "Ocean Cave",    SubTitle = "Remnants", TitleColor = Color.HotPink,     StrokeColor = Color.Black }},
+            {"Undergrowth",  new BiomeEntry{ Title = "Undergrowth",   SubTitle = "Remnants", TitleColor = Color.ForestGreen, StrokeColor = Color.Black }},
+            {"AerialGarden", new BiomeEntry{ Title = "Aerial Garden", SubTitle = "Remnants", TitleColor = Color.Turquoise,   StrokeColor = Color.Black }},
+            {"ForgottenTomb",new BiomeEntry{ Title = "Forgotten Tomb",SubTitle = "Remnants", TitleColor = Color.DimGray,     StrokeColor = Color.Black }},
+            {"MagicalLab",   new BiomeEntry{ Title = "Magical Lab",   SubTitle = "Remnants", TitleColor = Color.LightPink,   StrokeColor = Color.Black }},
+            {"EchoingHalls", new BiomeEntry{ Title = "Echoing Halls", SubTitle = "Remnants", TitleColor = Color.LightSlateGray, StrokeColor = Color.Black }},
+            {"Pyramid",      new BiomeEntry{ Title = "Pyramid",       SubTitle = "Remnants", TitleColor = Color.Goldenrod,   StrokeColor = Color.Black }},
+            {"Hive",         new BiomeEntry{ Title = "Giant Hive",    SubTitle = "Remnants", TitleColor = Color.Gold,        StrokeColor = Color.Black }},
+            {"JungleTemple", new BiomeEntry{ Title = "Jungle Temple", SubTitle = "Remnants", TitleColor = Color.Yellow,      StrokeColor = Color.Black }},
         };
 
         biomes = null;
